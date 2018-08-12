44d79319432cbc2c5452d14fd09c231d706154d4
SHEFFmachine <sheff@sheffmachine.com>
2018-08-11 20:17:15 -0700

    We now have a reducer working

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -4,6 +4,7 @@ const INITIAL_STATE = { email: '' }
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
+      return { email: action.payload }
       break
     default:
       return state
