033f3d4154c95abe9a604419e0cf993b0b7e4bbb
SHEFFmachine <sheff@sheffmachine.com>
2018-08-29 22:40:47 -0700

    wire up user auth with async action creator via redux thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 import { EMAIL_CHANGED, PASSWORD_CHANGED } from '../constants/constants'
 
 export const emailChanged = text => ({ type: EMAIL_CHANGED, payload: text })
@@ -5,3 +7,13 @@ export const passwordChanged = text => ({
   type: PASSWORD_CHANGED,
   payload: text,
 })
+
+export const loginUser = ({ email, password }) => {
+  return dispatch => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(user => dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user }))
+      .catch(err => console.log(err))
+  }
+}
