f46b0bfb4e29248f721c28207eedae189f26042a
SHEFFmachine <sheff@sheffmachine.com>
2018-08-30 21:44:23 -0700

    one last trick on resetting state

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -24,11 +24,8 @@ export default (state = INITIAL_STATE, action) => {
     case LOGIN_USER_SUCCESS:
       return {
         ...state,
+        ...INITIAL_STATE,
         user: action.payload,
-        loading: false,
-        error: '',
-        email: '',
-        password: '',
       }
     case LOGIN_USER_FAIL:
       return { ...state, error: action.payload, loading: false }
